c009f34d2df7d7ef38cf6171bfb64048f392454b
Rainer Stropek <rainer@timecockpit.com>
2025-10-10 08:39:58 +0200

    Saved in TypeScript Playground at 2025-10-10T06:39:58.144Z

--- flowers_basics/index.ts
+++ flowers_basics/index.ts
@@ -1,12 +1,24 @@
-// <<< ADD CONSTANTS HERE (if you need them)
+const CIRCLED = 80// <<< ADD CONSTANTS HERE (if you need them)
 
 function setup() {
   createCanvas(1000, 1000)
   background("white")
 
-  line
+    fill("lightgreen")
+  stroke("darkgreen")
+  circle(650,400,CIRCLED)
+
+    fill("lightgreen")
+  stroke("darkgreen")
+  circle(550,400,CIRCLED)
+
+    fill("lightgreen")
+  stroke("darkgreen")
+  circle(500,450,CIRCLED)
+
+  stroke("nofill")
   fill("yellow")
-  circle(700,400,65)
+  circle(600,400,65)
 
    // <<< ADD YOUR CODE HERE
 }
