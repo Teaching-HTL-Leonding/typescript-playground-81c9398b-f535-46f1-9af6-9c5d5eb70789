e1c421995004eff6b9f0386560a55dacf5cbfa43
Rainer Stropek <rainer@timecockpit.com>
2025-10-10 08:43:27 +0200

    Saved in TypeScript Playground at 2025-10-10T06:43:26.918Z

--- flowers_basics/index.ts
+++ flowers_basics/index.ts
@@ -4,21 +4,26 @@ function setup() {
   createCanvas(1000, 1000)
   background("white")
 
+    stroke("nofill")
+  fill("yellow")
+  circle(600,400,65)
+
     fill("lightgreen")
   stroke("darkgreen")
   circle(650,400,CIRCLED)
 
-    fill("lightgreen")
+   fill("lightgreen")
+  stroke("darkgreen")
+  circle(600,450,CIRCLED)
+
+      fill("lightgreen")
   stroke("darkgreen")
   circle(550,400,CIRCLED)
 
     fill("lightgreen")
   stroke("darkgreen")
-  circle(500,450,CIRCLED)
+  circle(600,350,CIRCLED)
 
-  stroke("nofill")
-  fill("yellow")
-  circle(600,400,65)
-
+arc()
    // <<< ADD YOUR CODE HERE
 }
