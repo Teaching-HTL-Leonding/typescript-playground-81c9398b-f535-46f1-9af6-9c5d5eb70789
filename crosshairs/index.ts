abcf0307568ba48d18857ea364b514b3cfd8fe88
Rainer Stropek <rainer@timecockpit.com>
2025-10-16 13:12:45 +0200

    Saved in TypeScript Playground at 2025-10-16T11:12:45.167Z

--- crosshairs/index.ts
+++ crosshairs/index.ts
@@ -5,8 +5,11 @@ function setup() {
 }
 
 function mouseMoved() {
- // background("lightblue")
+  background("lightblue")
   stroke("black")
   noFill()
-  circle(mouseX,mouseY,20)
+  circle(mouseX, mouseY, 20)
+  circle(mouseX, mouseY, 10)
+  line(mouseX + 20, mouseY, mouseX - 20, mouseY)
+  line(mouseX, mouseY + 20, mouseX, mouseY - 20)
 }
