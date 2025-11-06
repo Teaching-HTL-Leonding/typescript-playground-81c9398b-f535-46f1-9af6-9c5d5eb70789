48a03d85e29429d38f050e772fa49b763ed0da2c
Rainer Stropek <rainer@timecockpit.com>
2025-11-06 13:14:55 +0100

    Saved in TypeScript Playground at 2025-11-06T12:14:55.150Z

--- empty_playground/index.ts
+++ empty_playground/index.ts
@@ -1,5 +1,25 @@
 function setup() {
+    createCanvas(500, 500)
+    background("black")
+    stroke("green")
+    strokeWeight(3)
+    noFill()
 }
 
-function draw() {
+let lastClickX: number = 0
+let lastClickY: number = 0
+
+function mouseClicked() {
+    let a = mouseY - lastClickY
+    let b = mouseX - lastClickX
+    let c = sqrt(a * a + b * b)
+    //let cheat = dist(mouseX, mouseY, lastClickX, lastclickY)
+    let diameter = 2 * c
+    circle(mouseX, mouseY, diameter)
+    line(,diameter,mouseX,mouseY)
+    line()
+    line()
+    lastClickX = mouseX
+    lastClickY = mouseY
+
 }
