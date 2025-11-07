e3fc8835c2fb1216995886514f88632f85615dd3
Rainer Stropek <rainer@timecockpit.com>
2025-11-07 09:07:27 +0100

    Saved in TypeScript Playground at 2025-11-07T08:07:26.435Z

--- rect_tool_exam/index.ts
+++ rect_tool_exam/index.ts
@@ -23,7 +23,7 @@ function mouseMoved() {
     rect(width / 2, height / 2, w, h)
 
     line(mouseX, mouseY, mouseX + w, mouseY + h)
-    line(mouseX + w, mouseY + h, mouseX , mouseY)
+    line(mouseX , mouseY , mouseX , mouseY)
 
 
 
