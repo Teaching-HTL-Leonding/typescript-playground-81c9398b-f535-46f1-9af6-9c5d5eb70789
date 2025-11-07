d98676af5a8ba2f95d23c09b969bd13bc5a617ec
Rainer Stropek <rainer@timecockpit.com>
2025-11-07 08:23:07 +0100

    Saved in TypeScript Playground at 2025-11-07T07:23:07.570Z

---
+++ rect_tool_exam/index.ts
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(400, 200);
+    background("black");
+    angleMode(DEGREES);
+}
+    function mouseMoved() {
+    
+    background("black")
+
+}
+// <<< Add the function `mouseMoved` with the required code here
