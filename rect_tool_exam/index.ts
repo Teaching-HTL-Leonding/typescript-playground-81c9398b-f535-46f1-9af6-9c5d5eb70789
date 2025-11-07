4a7500a25f09ff6d2170da50fbb7c5209a9490cb
Rainer Stropek <rainer@timecockpit.com>
2025-11-07 08:34:05 +0100

    Saved in TypeScript Playground at 2025-11-07T07:34:05.273Z

--- rect_tool_exam/index.ts
+++ rect_tool_exam/index.ts
@@ -3,9 +3,20 @@ function setup() {
     background("black");
     angleMode(DEGREES);
 }
-    function mouseMoved() {
-    
+function mouseMoved() {
+
     background("black")
+
+    stroke("wight")
+    noFill()
+
+    rectMode(CENTER)
+    let w = (width / 2 - height / 2)
+    let h = (height / 2 - width / 2)
+
+    rect(width /2, height /2, w, h)
+    
+
 
 }
 // <<< Add the function `mouseMoved` with the required code here
