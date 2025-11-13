e5e9eed74008c7a230e14f09e638c3d80dd83d70
Rainer Stropek <rainer@timecockpit.com>
2025-11-13 13:30:50 +0100

    Saved in TypeScript Playground at 2025-11-13T12:30:50.081Z

--- empty_playground/index.ts
+++ empty_playground/index.ts
@@ -1,5 +1,19 @@
 function setup() {
+    createCanvas(300,300)
+    background("black")
+    colorMode(HSB)
+
 }
 
+let x : number = 0
+let colorHue: number = 0
+
 function draw() {
+    background("black")
+    noStroke()
+    fill(colorHue, 100, 100)
+    colorHue = (colorHue + 1) % 360
+    circle(x,x, x)
+    x = x + 1
+    x = x % 300
 }
