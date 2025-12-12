fe985a423f6efabfe953e741d73fc6d45cd09ce3
Rainer Stropek <rainer@timecockpit.com>
2025-12-12 09:18:21 +0100

    Saved in TypeScript Playground at 2025-12-12T08:18:20.958Z

--- empty_playground/index.ts
+++ empty_playground/index.ts
@@ -1,16 +1,5 @@
 function setup() {
-    createCanvas(400, 400)
-    background("lightblue")
 }
 
 function draw() {
-    background("lightblue")
-    stroke("red")
-    fill("darkblue")
-    rect(width / 2 - 50, height / 2 - 25, 100,50)
-
-    if (mouseX >= width / 2 - 50 && mouseX <= width / 2 + 50
-     && mouseY >= height / 2 - 25 && mouseY <= height / 2 +25) {
-        text("drinnen", 10 ,10)
-    }
 }
