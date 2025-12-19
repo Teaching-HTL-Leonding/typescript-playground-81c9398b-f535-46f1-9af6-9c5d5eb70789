7ce1f28cc9d9af40a3c06d1389bbc6b7c945ba57
Rainer Stropek <rainer@timecockpit.com>
2025-12-19 09:19:14 +0100

    Saved in TypeScript Playground at 2025-12-19T08:19:13.812Z

--- mesh/index.ts
+++ mesh/index.ts
@@ -1,7 +1,7 @@
 const SIZE = 400;  // Canvas size (square)
 const MARGIN = 50; // Margin between the edges and the rays.
 // This is also the distance between the rays.
-const RAND = SIZE - MARGIN
+const SIDE = SIZE - MARGIN
 
 function setup() {
   createCanvas(SIZE, SIZE);
@@ -13,7 +13,7 @@ function setup() {
   for (let b = MARGIN; b < SIZE; b += MARGIN) {
     for (let a = MARGIN, color = 0; a < SIZE; a += MARGIN, color += 60) {
       stroke(color, 100, 100)
-      line(MARGIN, a, RAND, b)
+      line(MARGIN, a, SIDE, b)
     }
   }
 }
