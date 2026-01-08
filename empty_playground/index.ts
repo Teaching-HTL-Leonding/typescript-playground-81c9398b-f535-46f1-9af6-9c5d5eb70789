9ff8d0785be51b01b6fa41d7359713b6d2450f6f
Rainer Stropek <rainer@timecockpit.com>
2026-01-08 13:08:52 +0100

    Saved in TypeScript Playground at 2026-01-08T12:08:52.512Z

--- empty_playground/index.ts
+++ empty_playground/index.ts
@@ -1,5 +1,11 @@
-function setup() {
-}
-
 function draw() {
+    const centerX = [100, 150, 200, 250, 300]
+    const centerY = [100, 150, 200, 250, 300]
+
+    createCanvas(500, 400)
+    background("white")
+
+    fill("blue")
+    for (let i = 0; i < centerX.length; i++)
+        circle(centerX[i], centerY[i], 50)
 }
